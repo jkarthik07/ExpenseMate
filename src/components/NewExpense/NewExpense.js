9b1071d21f611105ca3f8f9187773f600d0433dd
JK <jkarthik0703@gmail.com>
2024-10-08 11:55:41 +0530

    spend details with react

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = (props) => {
+  //Pass data from child to parent
+  const [addExpense, updatedAddExpense] = useState(false);
+
+  const newExpenseHandler = () => {
+    updatedAddExpense(true);
+  }
+  const cancelHandler = () => {
+    updatedAddExpense(false);
+  }
+
+
+  const saveExpenseData = (data) => {
+    if (data) {
+      const expenseData = {
+        ...data,
+        id: Math.random().toString()
+      };
+      if (props && props.saveExpense) {
+        props.saveExpense(expenseData);
+      }
+      console.log(expenseData, 'NewExpense');
+    }
+  }
+
+  // const [count, setCount] = useState(0);
+
+  // useEffect(() => {
+  //   setCount((count) => count + 1);
+  // }, [addExpense]);
+
+  return (
+    <div className='new-expense'>
+      {/* Conditional Rendering */}
+      {!addExpense && < button onClick={newExpenseHandler} > New Expense</button >}
+      {addExpense && <ExpenseForm saveNewExpense={saveExpenseData} onCancel={cancelHandler} />}
+    </div>
+  )
+}
+
+export default NewExpense;
